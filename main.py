91c23bb8f45e7c0aabdc534d6a95fb0acdb1e520
Mason Hall <masonhall@gmail.com>
2023-02-03 13:25:51 -0500

    small update

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     # Like this:
 
     for cast in fcc.stream_casts(max_counter=120):
-        print(cast)
-        if cast and "foo" in cast.text:
-            # fcc.post_cast("bar", parent=Parent(fid=cast.author.fid, hash=cast.hash))
-            pass
+        if cast:
+            print(cast.timestamp)
+            if "foo" in cast.text:
+                # fcc.post_cast("bar", parent=Parent(fid=cast.author.fid, hash=cast.hash))
+                pass
